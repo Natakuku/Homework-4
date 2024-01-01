2364b93d00c0b0576eeea76410592a77517d07ce
“Nataliia” <kukobnikova@gmail.com>
2024-01-01 17:50:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = +prompt("Type your first number", "");
+let b = +prompt("Type your second number", "");
+let c = (a + b);
+let d = (a - b);
+let e = (a * b);
+let f = (a / b);
+
+alert(a + '+' + b + '=' + c );
+alert(a + '-' + b + '=' + d );
+alert(a + '*' + b + '=' + e );
+alert(a + '/' + b + '=' + f );
